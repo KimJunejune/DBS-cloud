468470b5a1740e2b5c329f3e082df61866f60ac8
KimJunejune <82161125+KimJunejune@users.noreply.github.com>
2022-07-25 10:33:35 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import joblib
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        rates = float(request.form.get("rates"))
+        print(rates)
+
+        model1 = joblib.load("linear")
+        r1 = model1.predict([[rates]])
+
+        model2 = joblib.load("tree")
+        r2 = model2.predict([[rates]])
+
+        return render_template("index.html", result1=r1, result2=r2)
+    else:
+        # 未收到
+        return render_template("index.html", result1='waiting', result2='waiting')
+
+
+if __name__ == '__main__':
+    app.run()
